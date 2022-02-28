872ca5c3166a6e6042b7c3119c13fd8d486ce047
Tomas Manguel <tmanguel@mobilecomputing.com.ar>
2022-02-28 09:51:11 -0300

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
+require("@nomiclabs/hardhat-etherscan");
+
+
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    rinkeby: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  }
+};
